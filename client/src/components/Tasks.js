65d26b6ff38f27dd38e83bf888de3dbc4ceaa5e8
Siwagarn Jitwarodom <siwagarnj@gmail.com>
2021-04-26 23:05:10 +0700

    update login fun

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,5 +1,4 @@
 import Task from './Task'
-import {Link} from 'react-router-dom'
 
 const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
@@ -7,7 +6,6 @@ const Tasks = ({tasks, onDelete, onToggle}) => {
             {tasks.map((task) => (
                 <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
             ))}
-            <p><Link to="/" style={{float: 'right'}}>Log out</Link></p>
         </>
     )
 }
