14faacee6287f5c7df1b57fc697a60130fb12307
atiwat1113 <atiwat.deepo@gmail.com>
2021-04-27 00:33:45 +0700

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -26,7 +26,7 @@ router.post('/', (req, res) => {
             res.send('error has occured');
         } else {
             if(user === null){
-                res.send(user);
+                res.send({});
             }else {
                 req.session.username = user.username;
                 res.send(user);
