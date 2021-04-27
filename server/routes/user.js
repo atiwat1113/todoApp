6ac112e63b5aa291be259ec9ef86ee4f2dbbf2b6
atiwat1113 <atiwat.deepo@gmail.com>
2021-04-28 00:44:16 +0700

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -17,7 +17,6 @@ router.get('/users', (req, res) => {
 // sign in
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
-        console.log(user);
         if(err) throw err;
         if(!user) res.send('');
         else { 
